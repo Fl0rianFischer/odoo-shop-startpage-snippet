1f022d50f1ef784c0a83ac0d7396157056df3765
Florian Fischer <ffischer@bloopark.de>
2015-04-09 17:11:12 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Shop Landing Page Snippet",
+
+    'summary': """
+    Provide a fast and beautiful orientation for customers""",
+
+    'description': """
+    This Addon provides a small shop landing page""",
+
+    'author': "Florian Fischer",
+    'website': "http://www.bloopark.de",
+
+    'category': 'Content',
+    'version': '1.0',
+
+    'depends': [
+        'base',
+        'website'
+    ],
+    'data': [
+        'views/landing_page.xml'
+    ],
+
+    'demo': [
+    ],
+
+    'tests': [
+    ],
+}
